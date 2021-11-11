8447e0d4f58861d86aebb54848b465531faf7726
kitchen-boy <kaye.chen.boyce@gmail>
2021-11-11 06:38:58 -0500

    Capture Form Field Values

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,19 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to dynamically create the task item
 var createTaskHandler = function(event) {
+
   event.preventDefault();
-  // to prevent browser from default behaviour of refershing page
+  // to prevent browser from default behaviour of refreshing page
+
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+ 
  
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  listItemEl.textContent = taskNameInput;
   tasksToDoEl.appendChild(listItemEl);
 };
 
 // an event listener statment = "on submit, create a task"
 // targeting the form element
 formEl.addEventListener("submit", createTaskHandler);
-
-/* examples of 2 long anonymous event listener functions replaced by preceding code
- which uses createTaskhandler as callback function */
-/* buttonEl.addEventListener("click", function() {
-  var taskItemEl = document.createElement("li");
-  taskItemEl.className = "task-item";
-  taskItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(taskItemEl);
-}); */
-/* buttonEl.addEventListener("click", function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-});*/
-
-
