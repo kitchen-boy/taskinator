22c8b2f1b970e37864c2d0b4b88e8f2983c6b2f1
kitchen-boy <kaye.chen.boyce@gmail>
2021-11-12 08:23:32 -0500

    Refactored code to organize funtionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,17 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
   
+  // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput, 
+    type: taskTypeInput
+  };
+
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
   //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -21,12 +32,11 @@ var taskFormHandler = function(event) {
   taskInfoEl.className = "task-info";
 
   // add HTML content to div
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
 
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
-
 /* an event listener statment = "on submit, create a task" targeting the form element */
 formEl.addEventListener("submit", taskFormHandler);
