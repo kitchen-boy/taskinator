703a359ebeaaed9a4b24a47c4460d315125a09a7
kitchen-boy <kaye.chen.boyce@gmail>
2021-11-12 07:32:20 -0500

    Safety-check code to capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var createTaskHandler = function(event) {
 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  console.log(taskTypeInput);
+  
   //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -19,17 +19,14 @@ var createTaskHandler = function(event) {
   var taskInfoEl = document.createElement("div")
   // give it a class name
   taskInfoEl.className = "task-info";
+
   // add HTML content to div
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-  
   listItemEl.appendChild(taskInfoEl);
 
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
-
-  console.dir(listItemEl);
 };
 
-// an event listener statment = "on submit, create a task"
-// targeting the form element
+/* an event listener statment = "on submit, create a task" targeting the form element */
 formEl.addEventListener("submit", createTaskHandler);
